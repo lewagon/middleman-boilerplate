780e912a2861409d01814e86ca5a91cf071988d4
Sebastien Saunier <seb@saunier.me>
2015-01-16 16:25:51 +0100

    Deploy to GitHub Pages with middleman-deploy gem

--- config.rb
+++ config.rb
@@ -70,3 +70,15 @@ configure :build do
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
 end
+
+# Deployment
+activate :deploy do |deploy|
+  deploy.method = :git
+  deploy.build_before = true
+
+  # Optional Settings
+  # deploy.remote = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
